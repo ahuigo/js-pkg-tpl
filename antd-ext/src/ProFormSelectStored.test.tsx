d0b1659342bc02e828398fe84f6d840263e0615a
ahuigo <1781999+ahuigo@users.noreply.github.com>
2024-12-09 16:02:00 +0800

    fix: missing react test dom

--- antd-ext/src/ProFormSelectStored.test.tsx
+++ antd-ext/src/ProFormSelectStored.test.tsx
@@ -1,16 +1,29 @@
 import * as React from 'react';
+import "./tests/mocks";
 import '@testing-library/jest-dom';
 import { render, fireEvent } from '@testing-library/react';
+import { screen } from '@testing-library/dom';
+import { ProForm, } from "@ant-design/pro-components";
 import { ProFormSelectStored } from './ProFormSelectStored';
+
 const props = {
   name: 'test',
+  api_key: 'test',
   value: 'frame',
-  label: 'Test Label',
-  initialValue: 'frame',
+  label: 'Frame1',
+  initialValue: ['frame1'],
   onChange: jest.fn(),
 };
 
-it('renders correctly with given props', () => {
-  const { getByLabelText } = render(<ProFormSelectStored {...props} />);
-  expect(getByLabelText('Test Label')).toBeInTheDocument();
+function RenderProFormSelectStored() {
+  return (<ProForm>
+    <ProFormSelectStored {...props} />
+  </ProForm>);
+}
+it('renders correctly with given props', async () => {
+  const rtn = render(<RenderProFormSelectStored />);
+  // console.log(rtn);
+  expect((await screen.queryAllByText('Frame1'))[0]).toBeInTheDocument();
+  expect((await rtn.queryAllByText('Frame1'))[0]).toBeInTheDocument();
+  // expect(await screen.queryByText('Frame1')).toBeVisible();
 });
--- antd-ext/src/ProFormSelectStored.tsx
+++ antd-ext/src/ProFormSelectStored.tsx
@@ -1,30 +1,40 @@
 import React from 'react';
-import { ProFormSelect } from '@ant-design/pro-components';
 import { Spin, Form, Input } from 'antd';
-interface Props {
-  name: string;
-  value: string;
-  label?: string;
-  initialValue: string;
-  onChange: (v: string) => void;
+import { ProFormSelect, ProFormSelectProps } from '@ant-design/pro-components';
+interface ProFormSelectStoredProps extends Omit<ProFormSelectProps, 'options'> {
+  /** 2. 用下划线命名，避免与标准库Props冲突？ */
+  api_key: string;
+  api_endpoint?: string;
+  search_mode?: 'api' | 'native';// api: 通过api搜索(默认)，native: 本地搜索
+
 }
-const options = [{ value: 'frame', label: 'Frame' }, { value: 'frame2', label: 'Frame2' },];
-export function ProFormSelectStored(props: Props) {
+const options = [{ value: 'frame1', label: 'Frame1' }, { value: 'frame2', label: 'Frame2' },];
+export function ProFormSelectStored(props: ProFormSelectStoredProps) {
   const loading = false;
   return <ProFormSelect
     name={props.name}
     label={props.label}
+    width={150}
     options={options}
     initialValue={props.initialValue}
     fieldProps={{
       mode: 'multiple',
       showSearch: true,
-      placeholder: '*wildcard search*',
+      placeholder: '*模糊wildcard搜索*',
       optionFilterProp: 'label', // This is needed to make the search work with label
       notFoundContent: loading ? <Spin size="small" /> : null, // Add this line
       onSearch: (v: string) => {
         console.log(v);
       },
+      onInputKeyDown: (e) => {
+        const target = e.target as HTMLInputElement;
+        console.log({ 'k': e.key, v: target.value });
+        if (e.key === 'Enter') {
+          e.stopPropagation(); // <-- this is the trick
+          //@ts-ignore
+          addOption(e.target.value);
+        }
+      }
     }}
   />;
 };
